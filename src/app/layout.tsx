93b9789f1cdbb5c8670b0c398cfcfea6e85e95ee
jaeungjayjang <wodndckato@gmail.com>
2025-07-11 11:22:05 -0400

    feat: add Analytics component to RootLayout for enhanced tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,6 +65,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
